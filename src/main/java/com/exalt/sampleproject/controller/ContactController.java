d19cda0d0bb27a8ee08fd1948b18642458c02610
Sami Imran <samiimran2016@gmail.com>
2021-08-19 11:44:30 +0300

    put some comments on CRUD methods

--- src/main/java/com/exalt/sampleproject/controller/ContactController.java
+++ src/main/java/com/exalt/sampleproject/controller/ContactController.java
@@ -31,8 +31,9 @@ public class ContactController {
      */
     @GetMapping(path = "/restaurants/{restaurantId}/contacts", produces = {"application/json"})
     public Contact listContactsByRestaurantId(@PathVariable Long restaurantId) {
-        if (findRestaurant(restaurantId).isPresent()) {
-            Optional<Restaurants> optionalRestaurant = findRestaurant(restaurantId);
+        Optional<Restaurants> optionalRestaurant = findRestaurant(restaurantId);
+
+        if (optionalRestaurant.isPresent()) {
             return optionalRestaurant.get().getContact();
         } else {
             throw new ResourceNotFoundException("Restaurant id " + restaurantId + ", not found");
@@ -43,18 +44,29 @@ public class ContactController {
      *
      * @param contact new contact object to be created
      * @param restaurantId id for the restaurant
-     * @return contact created
+     * @return message status
      */
     @PostMapping(path = "restaurants/{restaurantId}/contacts", produces = {"application/json"})
     public ResponseMessage createContact(@RequestBody Contact contact, @PathVariable Long restaurantId) {
         return contactService.createContact(contact, findRestaurant(restaurantId));
     }
 
+    /**
+     *
+     * @param contact the updated one
+     * @param restaurantId id for the restaurant to de edited
+     * @return message status
+     */
     @PutMapping(path = "restaurants/{restaurantId}/contacts", produces = {"application/json"})
     public ResponseMessage updateContact(@RequestBody Contact contact, @PathVariable Long restaurantId) {
         return contactService.updateContactByRestuarant(findRestaurant(restaurantId), contact);
     }
 
+    /**
+     *
+     * @param restaurantId for contact to be deleted
+     * @return message status
+     */
     @DeleteMapping(path = "restaurants/{restaurantId}/contacts", produces = {"application/json"})
     public ResponseMessage deleteContact(@PathVariable Long restaurantId) {
         return contactService.deleteContactByRestaurant(findRestaurant(restaurantId));
