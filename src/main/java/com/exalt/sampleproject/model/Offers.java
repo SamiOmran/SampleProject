c9b83029d27f60d02f828dfa4ba4f90d5d83ca46
Sami Imran <samiimran2016@gmail.com>
2021-08-23 17:12:36 +0300

    Create Offers model

--- src/main/java/com/exalt/sampleproject/model/Offers.java
+++ src/main/java/com/exalt/sampleproject/model/Offers.java
@@ -1,4 +1,29 @@
 package com.exalt.sampleproject.model;
 
-public class Offers extends Auditable{
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class Offers {
+
+    @Id @JsonProperty
+    @GeneratedValue
+    private Long id;
+
+    @NonNull @JsonProperty
+    private LocalDate localDate = LocalDate.now();
+
+    @NonNull @JsonProperty
+    private String offer;
+
+    @ManyToOne @JoinColumn(name = "restaurant_id")
+    @JsonProperty
+    private Restaurants restaurants;
 }
