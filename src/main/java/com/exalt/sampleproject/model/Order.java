705dc24c190b8af44a3ef5a4199a3749192e8ad0
Sami Imran <samiimran2016@gmail.com>
2021-08-23 10:19:48 +0300

    .

--- src/main/java/com/exalt/sampleproject/model/Order.java
+++
@@ -1,4 +0,0 @@
-package com.exalt.sampleproject.model;
-
-public class Order extends Auditable{
-}
--- src/main/java/com/exalt/sampleproject/repository/LocationRepo.java
+++
@@ -1,7 +0,0 @@
-package com.exalt.sampleproject.repository;
-
-import com.exalt.sampleproject.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepo extends JpaRepository<Location, Long> {
-}
