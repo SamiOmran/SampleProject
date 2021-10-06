2809fad6d984e7d575d1d7a299fce45687df7a42
Sami Imran <samiimran2016@gmail.com>
2021-10-06 11:49:40 +0300

    --

--- src/main/java/com/exalt/sampleproject/SampleProjectApplication.java
+++ src/main/java/com/exalt/sampleproject/SampleProjectApplication.java
@@ -1,30 +1,12 @@
 package com.exalt.sampleproject;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SampleProjectApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(SampleProjectApplication.class, args);
-    }
-
-    //@Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext context) {
-        return args -> {
-            System.out.println("Let’s inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = context.getBeanDefinitionNames();
-            for (String bean: beanNames) {
-                System.out.println(bean);
-            }
-        };
     }
 
 }
